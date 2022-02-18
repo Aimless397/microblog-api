27eb2cd8d5cde43e1d0e01952b2b9b682086cf24
Xrls9 <jagox28@outlook.com>
2022-02-18 16:25:32 -0500

    chore: user routes documentation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-/* import 'reflect-metadata' */
 import express, { Application, Request, Response, NextFunction } from 'express';
-import { router } from './router'
+import { router } from './router';
+import YAML from 'yamljs';
+import swaggerUI from "swagger-ui-express";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
+const swaggerDocument = YAML.load('api.yaml');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +14,8 @@ app.get('/api/v1/status', (req: Request, res: Response) => {
   res.json({ time: new Date() });
 });
 app.use('/', router(app));
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(PORT, async () => {
   console.log(`Server listening on port ${PORT}`);
