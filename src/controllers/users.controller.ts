dacc696ebfae096916bf419aaae946a81acc7963
Xrls9 <jagox28@outlook.com>
2022-02-25 02:18:08 -0500

    feat:sendgrid service

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,7 +2,9 @@ import { User } from '@prisma/client';
 import { plainToClass } from 'class-transformer';
 import { Request, Response } from 'express';
 import { CreateUserDto } from '../dtos/users/request/create-user.dto';
+import { AuthService } from '../services/auth.service';
 import { UsersService } from '../services/users.service';
+import { SendgridService } from '../services/sendgrid.service'
 
 export async function find(req: Request, res: Response): Promise<void> {
   const result = await UsersService.find();
@@ -48,4 +50,19 @@ export async function update(req: Request, res: Response): Promise<void> {
   /* const result =  await UsersService.find(); */
 
   res.status(200).json('update');
+};
+
+export async function sendEmail(req: Request, res: Response): Promise<void> {
+  const token = AuthService.generateAccessToken(req.uuid as string)
+  try{
+    await SendgridService.sendEmail({
+      to: req.email,
+      subject: "Account Verification",
+      html: `<strong>Token: ${token}</strong>`,
+    })
+  }catch (e){
+    console.log(e)
+  }
+  
+  res.status(204);
 };
